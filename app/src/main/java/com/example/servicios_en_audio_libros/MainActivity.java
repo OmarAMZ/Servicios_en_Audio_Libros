d7592ac1eeb941c2fd90e81e3275df27b3da179a
OmarAMZ <s16120319@alumnos.itsur.edu.mx>
2021-05-18 23:36:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/servicios_en_audio_libros/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.servicios_en_audio_libros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
